0f55dcf059dc3f830d848c8cbcc9c939572d9534
777 <coin86@mail.ru>
2023-02-06 17:35:23 +0300

    ДЗ 6 Циклы

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Домашнее задание по теме Циклы");
+
+        System.out.println("Задача № 1");
+
+        for (int i= 0; i<=10; i=i+1){
+            System.out.println("Итерация цикла " + i);
+        }
+        System.out.println("Задача № 2");
+
+            for (int b= 10; b>=0; b=b-1){
+                System.out.println("Итерация цикла " + b);
+            }
+        System.out.println("Задача № 3");
+
+        for (int a= 0; a<=17 ; a=a+2){
+            System.out.println("Четные числа " + a);
+        }
+
+        System.out.println("Задача № 4");
+
+        for (int с= 10; с>=-10; с=с-1){
+            System.out.println("Итерация цикла " + с);
+        }
+
+
+
+    }
+}
